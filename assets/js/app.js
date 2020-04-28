bdc826c2e83c6aed100b385b015af9bf16730c41
Adnan Niaz <adnanniaz77@yahoo.com>
2020-04-28 09:07:46 -0700

    add to github

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     console.log("hi");
+
+//     // $(".call").on("click", function (e) {
+//     //     let copiedWord = $(".fa-phone")[0].attributes.data.value;
+//     //     copiedWord.select();
+//     //     document.execCommand("copy");
+//     // });
+// });
